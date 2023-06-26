f8feafd6eab6527b8577fd14b1b4a417b95b3dcb
Dikesh Kumar <117905520+Dikesh01@users.noreply.github.com>
2023-06-26 23:34:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 //your JS code here. If required.
+
+
+const submitBtn = document.getElementById('submit');
+
+submitBtn.addEventListener('click', getDetails)
+
+function getDetails(event){
+	event.preventDefault();
+	const userName = document.getElementById('username').value;
+	const password = document.getElementById('password').value;
+	const remember = document.getElementById('checkbox').checked;
+
+	if(remember){
+		localStorage.setItem('username', userName);
+		localStorage.setItem('password', password);
+	}
+	else{
+		localStorage.removeItem('username');
+		localStorage.removeItem('password');
+	}
+	
+	alert(`Logged in as ${ userName}`);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
